56c74ed5edbb33b7544909500fa830e9b7f0d672
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-21 15:54:11 +0530

    modified statements.py

--- Fundamentals/statements.py
+++ Fundamentals/statements.py
@@ -1,5 +1,6 @@
 age = 45
-
+my_age = 25
+your_age = 18
 
 if age < 18:
     print("You are too young!")
@@ -7,3 +8,9 @@ elif age == 18:
     print("You can enter the club but cannot not drink!!!")
 else:
     print("You can enter the club and access all the facilities!")
+
+
+if my_age > 20 and your_age < 15:
+    print("We are old enough!")
+else:
+    print("Not old enough!")
