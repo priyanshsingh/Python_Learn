72df03cd71244a5a787596630d5c4f6c566ae3bb
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-21 13:58:36 +0530

    added strings.py

--- Fundamentals/strings.py
+++ Fundamentals/strings.py
@@ -8,5 +8,11 @@ print(length)
 fourth_index = language[4]
 print(fourth_index)
 
+negative_index = language[-1]
+print(negative_index)
+
 multiple_indeces = language[0:3]
 print(multiple_indeces)
+
+multiple_indeces2 = language[1:]
+print(multiple_indeces2)
