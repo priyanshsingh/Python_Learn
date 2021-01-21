687fda4fb77220d1ab08f1af72cb38817d390d5c
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-21 13:39:43 +0530

    added strings.py

---
+++ Fundamentals/strings.py
@@ -0,0 +1,12 @@
+language = "Python"
+
+length = (len(language))
+print(length)
+
+# Access Each element of the string
+
+fourth_index = language[4]
+print(fourth_index)
+
+multiple_indeces = language[0:3]
+print(multiple_indeces)
