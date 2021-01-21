448e39fba6a3830d65ef062ac52dd8056fd85ea4
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-21 14:04:39 +0530

    modified string_methods.py

---
+++ Fundamentals/string_methods.py
@@ -0,0 +1,12 @@
+language = "Python Is A Cool LanGuAge"
+# String Functions
+
+upper = language.upper()
+lower = language.lower()
+find = language.find("Cool")
+replace = language.replace("Cool", "Good")
+
+print(upper)
+print(lower)
+print(find)
+print(replace)
