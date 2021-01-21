75ee48f9885f91fe7d7b654e56d003e5354962f4
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-21 15:51:10 +0530

    added statements.py

---
+++ Fundamentals/statements.py
@@ -0,0 +1,9 @@
+age = 45
+
+
+if age < 18:
+    print("You are too young!")
+elif age == 18:
+    print("You can enter the club but cannot not drink!!!")
+else:
+    print("You can enter the club and access all the facilities!")
