59ef11eb0eb1cc8ad8f78c3e1e37f30ba685a6fc
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-21 13:21:53 +0530

    modified concatination.py

--- Fundamentals/concatination.py
+++ Fundamentals/concatination.py
@@ -3,3 +3,7 @@ last_name = 'Singh'
 
 
 print("Full name is : " + first_name + ' ' + last_name)
+
+greeting = f"Hello, My name is {first_name} {last_name}. And I like to play guitar!!!"
+
+print(greeting)
