4075487725126da2ec397eb31c0f8406056aedd9
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-23 22:37:02 +0530

    modified imute.py

--- Fundamentals/imute.py
+++ Fundamentals/imute.py
@@ -1,9 +1,13 @@
 fruit = ["Apple", "Banana", "Kiwi", "Orange"]
 
-other_fruits = fruit
-
-other_fruits.append('Dragon')
-
-print(other_fruits)
+copied_fruits = fruit.copy()
+copied_fruits.append("Pineapple")
 
 print(fruit)
+print(copied_fruits)
+
+other_fruits = fruit
+other_fruits.append('Dragon')
+
+print(fruit)
+print(other_fruits)
