d65780025c938b67da18d5894d275612c28aa81e
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-23 19:15:42 +0530

    modified add remove list items

--- Fundamentals/add_remove_lists.py
+++ Fundamentals/add_remove_lists.py
@@ -1,6 +1,13 @@
+# adding items
+
 items = ['cars', 'bikes', 'helicopter', 'jet']
 print(items)
 items.append("Aiplane")
 print(items)
 items.insert(2, "Boat")
+print(items)
+
+# removing items
+
+items.pop()
 print(items)
